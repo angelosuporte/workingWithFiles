5590ab8f2e8ea29d88f166483474fd65cacf0f69
Angelo Carvalho <angelosuporteti@gmail.com>
2021-01-25 11:27:10 -0300

    fileInfo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.IO;
 
-namespace ConsoleApp1
+namespace WorkingFiles
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourcePath = @"c:\teste\file1.txt";
+            string targetPath = @"c:\teste\copyOffile1.txt";
+
+            try
+            {
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+
+            }
+            catch (IOException e)
+            {
+
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
