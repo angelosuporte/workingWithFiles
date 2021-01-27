c41a04b162ff2ba14474d1cf9adbe99314b799d1
Angelo Carvalho <angelosuporteti@gmail.com>
2021-01-27 14:18:16 -0300

    Execise with file

---
+++ ConsoleApp1/Entities/Sale.cs
@@ -0,0 +1,21 @@
+﻿namespace WorkingFiles.Entities
+{
+    class Sale
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Sale(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Total()
+        {
+             return Price * Quantity;
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Globalization;
+using WorkingFiles.Entities;
 
 namespace WorkingFiles
 {
@@ -7,18 +9,45 @@ namespace WorkingFiles
     {
         static void Main(string[] args)
         {
-            string path = @"c:\teste";
+            //exercise
+            string sales = @"c:\test\sales.csv";
 
-            Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
-            Console.WriteLine("PathSeparador: " + Path.PathSeparator);
-            Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
-            Console.WriteLine("GetFileName: " + Path.GetFileName(path));
-            Console.WriteLine("GetExtension: " + Path.GetExtension(path));
-            Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
-            Console.WriteLine("GetTempPath: " + Path.GetTempPath());
-            Console.WriteLine("Enter");
-            Console.ReadKey();
+            try
+            {
+                string[] salelines = File.ReadAllLines(sales);
+
+                string sourceFolderPath = Path.GetDirectoryName(sales);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summarySales.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in salelines)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Sale sale = new Sale(name, price, quantity);
+
+                        sw.WriteLine(sale.Name + "," + sale.Total().ToString("F2", CultureInfo.InvariantCulture));
+
+                    }
+                }
+                Console.WriteLine("Enter");
+                Console.ReadKey();
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+
 
         }
     }
